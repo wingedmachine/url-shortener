707e8feb540c7900495b278218911bba20eac8a0
wingedmachine <wheeler.pryor@gmail.com>
2019-06-25 15:33:02 -0400

    add index decoding

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -4,17 +4,18 @@ class ShortUrl < ApplicationRecord
   SHORT_URL_CHARS = "0123456789bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ".chars
 
   def encode
-    encode_index
-    add_checksum
+    self.class.encode_index(self.index)
+    get_checksum(self.url)
   end
 
   def decode
     remove_checksum
-    decode_index
+    self.class.decode_index
   end
 
   def self.encode_index(n)
     base = SHORT_URL_CHARS.size
+
     output = ""
     while n > 0
       output.prepend(SHORT_URL_CHARS[n.modulo(base)])
@@ -23,6 +24,11 @@ class ShortUrl < ApplicationRecord
     output
   end
 
-  def self.decode_index
+  def self.decode_index(s)
+    base = SHORT_URL_CHARS.size
+    
+    i = 0
+    s.each_char { |c| i = i * base + SHORT_URL_CHARS.index(c) }
+    i
   end
 end
--- spec/models/short_url_spec.rb
+++ spec/models/short_url_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe ShortUrl do
     expect(ShortUrl.encode_index(50)).to eq("10")
     expect(ShortUrl.encode_index(147904)).to eq("1984")
   end
+
+  it "correctly decodes indices" do
+    expect(ShortUrl.decode_index("1")).to eq(1)
+    expect(ShortUrl.decode_index("Z")).to eq(49)
+    expect(ShortUrl.decode_index("10")).to eq(50)
+    expect(ShortUrl.decode_index("1984")).to eq(147904)
+  end
 end
