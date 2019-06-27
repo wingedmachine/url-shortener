4abb7d0da170fa8ec8242564553f95df957aaac9
wingedmachine <wheeler.pryor@gmail.com>
2019-06-27 00:56:20 -0400

    create skeleton of ShortUrlsController

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,3 +1,14 @@
 class ShortUrlsController < ApplicationController
-  
+  def new
+  end
+
+  def create
+  end
+
+  def redirect
+    @url = ShortUrl.lengthen(params[:shortened_url])
+    render "new" unless @url
+
+    puts @url
+  end
 end
