d005d9e3e59b1216fdb9f75e157694c011163927
wingedmachine <wheeler.pryor@gmail.com>
2019-06-28 15:33:28 -0400

    allow creation of ShortUrls via webform

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -4,10 +4,27 @@ class ShortUrlsController < ApplicationController
   end
 
   def create
+    if @short_url = ShortUrl.create(url: short_url_params[:url])
+      flash[:shrink_amount] = "Your #{@short_url.url.length} character url has "\
+        "been shrunk down to be only #{@short_url.short.length} characters!"
+      flash[:short_url] = "http://localhost:3000/#{@short_url.short}"
+    else
+      flash.alert = @short_url.errors.full_messages.to_sentence
+    end
+    redirect_to root_path
   end
 
   def redirect
     @url = ShortUrl.lengthen(params[:shortened_url])
-    @url ? (redirect_to @url) : (render "new")
+    @url ? (redirect_to @url) : (
+      flash.alert = "Sorry, but we don't recognize that shortened url."
+      redirect_to root_path
+    )
+  end
+
+  private
+
+  def short_url_params
+    params.require(:short_url).permit(:url)
   end
 end
